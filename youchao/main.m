22539c79c6822de3ad7462ad23ed7059ab93c7c6
张威 <Kin@zhangweidemacbook-pro.local>
2015-12-01 14:03:54 +0800

    Initial Commit

---
+++ youchao/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  youchao
+//
+//  Created by Kirito on 15/12/1.
+//  Copyright © 2015年 Kirito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
